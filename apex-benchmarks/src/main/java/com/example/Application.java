0570bd592176f536c3d2075b75e8642fae597086
sandeshh <sandesh.hegde@gmail.com>
2016-01-27 10:21:06 -0800

    java serialize

--- apex-benchmarks/src/main/java/com/example/Application.java
+++ apex-benchmarks/src/main/java/com/example/Application.java
@@ -163,25 +163,6 @@ public class Application implements StreamingApplication
     }
   }
 
-    public CampaignProcessorCommon read (Kryo kryo, Input input, Class<CampaignProcessorCommon> type) {
-      ObjectInputStream in = null;
-      CampaignProcessorCommon e = null;
-
-      try {
-        in = new ObjectInputStream(input);
-         e = (CampaignProcessorCommon) in.readObject();
-        in.close();
-      } catch (IOException ioException) {
-        ioException.printStackTrace();
-      } catch (ClassNotFoundException e1) {
-        e1.printStackTrace();
-      }
-
-      return e;
-    }
-  }
-
-
   public static class CampaignProcessor extends BaseOperator
   {
     @FieldSerializer.Bind(JavaSerializer.class)
