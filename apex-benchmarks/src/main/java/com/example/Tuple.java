666ad1cf39f3ca1f28dc45cc1a9198a35e406816
sandeshh <sandesh.hegde@gmail.com>
2016-01-27 16:38:57 -0800

    Apex Application for benchmarking

---
+++ apex-benchmarks/src/main/java/com/example/Tuple.java
@@ -0,0 +1,28 @@
+/**
+ * Licensed under the terms of the Apache License 2.0. Please see LICENSE file in the project root for terms.
+ */
+
+package com.example;
+
+import java.io.Serializable;
+
+public class Tuple implements Serializable
+{
+  public String adId;
+  public String campaignId;
+
+  public String event_time;
+  public String clicks ;
+
+  public Tuple()
+  {
+  }
+
+  public Tuple(String adId, String campaignId, String eventTime, String clicks)
+  {
+    this.adId = adId;
+    this.campaignId = campaignId;
+    this.event_time = eventTime;
+    this.clicks = clicks;
+  }
+}
